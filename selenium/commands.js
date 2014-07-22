894438931c0a6c104e50331e426d6a1dbdcbff4b
viju-mathew <vijujm@gmail.com>
2014-07-21 19:55:02 -0700

    tab to 2 spaces

--- selenium/commands.js
+++ selenium/commands.js
@@ -6,77 +6,77 @@ var ScriptRunner = require('./script_runner');
 var Namespaces = {};
 
 function CommandNamespace(name) {
-    this.name = name;
-    this.actions = {};
+  this.name = name;
+  this.actions = {};
 
-    this.addAction = function(action) {
-        this.actions[action.name] = action;
+  this.addAction = function(action) {
+    this.actions[action.name] = action;
 
-        this[action.name] = action.run.bind(action);
-    }
+    this[action.name] = action.run.bind(action);
+  }
 }
 
 function CommandAction(command) {
-    extend(this, command);
+  extend(this, command);
 
-    this.name = this.action;
+  this.name = this.action;
 
-    var requiredOptions = this.options.required;
-    var optionalOptions = this.options.optional;
+  var requiredOptions = this.options.required;
+  var optionalOptions = this.options.optional;
 
-    this.run = function(userOptions) {
-        if (!userOptions) {
-            userOptions = {};
-        }
-
-        requiredOptions.forEach(function(option) {
-            if(userOptions[option.key] === undefined) {
-                throw new Error("Need to supply option " + option.key);
-            }
-
-            var userValue = userOptions[option.key];
-            if (option.values) {
-                // check that the user supplied valid value
-                if (option.values.indexOf(userValue) === -1) {
-                    throw new Error("Need to supply valid option for " + option.key);
-                }
-            }
-        });
-
-        if (this.name === "create") {
-            var appTypePrefix = userOptions.category.substring(0, 4);
-            if (userOptions.app_type === "GAME" && appTypePrefix !== "GAME") {
-                throw new Error("Need to supply valid category for app_type GAME");
-            }
-
-            if (userOptions.app_type !== "GAME" && appTypePrefix === "GAME") {
-                throw new Error("Need to supply valid category for app_type APPLICATION");
-            }
-        }
-
-        //default values assigned
-        optionalOptions.forEach(function(option) {
-            if (userOptions[option.key] === undefined){
-                userOptions[option.key] = option.default;
-            }
-        });
-
-        // run the actual script
-
-        ScriptRunner.run(this.namespace, this.name, userOptions);
+  this.run = function(userOptions) {
+    if (!userOptions) {
+      userOptions = {};
     }
+
+    requiredOptions.forEach(function(option) {
+      if(userOptions[option.key] === undefined) {
+        throw new Error("Need to supply option " + option.key);
+      }
+
+      var userValue = userOptions[option.key];
+      if (option.values) {
+        // check that the user supplied valid value
+        if (option.values.indexOf(userValue) === -1) {
+          throw new Error("Need to supply valid option for " + option.key);
+        }
+      }
+    });
+
+    if (this.name === "create") {
+      var appTypePrefix = userOptions.category.substring(0, 4);
+      if (userOptions.app_type === "GAME" && appTypePrefix !== "GAME") {
+        throw new Error("Need to supply valid category for app_type GAME");
+      }
+
+      if (userOptions.app_type !== "GAME" && appTypePrefix === "GAME") {
+        throw new Error("Need to supply valid category for app_type APPLICATION");
+      }
+    }
+    
+    //default values assigned
+    optionalOptions.forEach(function(option) {
+      if (userOptions[option.key] === undefined){
+        userOptions[option.key] = option.default;
+      }
+    });
+
+    // run the actual script
+
+    ScriptRunner.run(this.namespace, this.name, userOptions);
+  }
 }
 
 _commands.forEach(function(command) {
-    var namespaceName = command.namespace;
-    var namespace = Namespaces[namespaceName];
-    if (!namespace) {
-        namespace = new CommandNamespace(namespaceName);
-        Namespaces[namespaceName] = namespace;
-    }
+  var namespaceName = command.namespace;
+  var namespace = Namespaces[namespaceName];
+  if (!namespace) {
+    namespace = new CommandNamespace(namespaceName);
+    Namespaces[namespaceName] = namespace;
+  }
 
-    var action = extend(new CommandAction(command));
-    namespace.addAction(action);
+  var action = extend(new CommandAction(command));
+  namespace.addAction(action);
 });
 
 exports = module.exports = Namespaces;
