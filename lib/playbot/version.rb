7cf9afa332e65686a42e6310208d7531bcd633f5
viju-mathew <vijujm@gmail.com>
2014-07-31 14:58:49 -0700

    remove ruby stuff and move selenium/ to lib/

--- lib/playbot/version.rb
+++
@@ -1,5 +0,0 @@
-class PlayBot
-  VERSION = "0.0.1"
-  DESCRIPTION = 'A robot for Google Play tasks'
-  WEBSITE = 'https://github.com/usepropeller/playbot'
-end
--- vendor/botlib/README.md
+++
@@ -1,3 +0,0 @@
-# BotLib
-
-The shared engine between AppleBot and PlayBot. Not meant to be used as a stand-alone gem.
